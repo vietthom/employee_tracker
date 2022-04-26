e16a8db30790131e7246d1ff95fdc12051492504
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-04-26 07:42:01 -0600

    establish server with cms prompts

--- server.js
+++ server.js
@@ -25,3 +25,71 @@ displayBanner = () =>{
     console.log("***********************************")
     mainMenu();
 };
+
+const mainMenu=()=>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'select',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Update an employee manager',
+                'View employees by department',
+                'Delete a department',
+                'Delete a role',
+                'Delete an employee',
+                'View department budgets',
+                'Exit'
+            ]
+        }
+    ])
+    .then((answer)=>{
+        switch(answer.select){
+            case 'View all departments':
+                showDepartments();
+                break;
+            case 'View all roles':
+                showRoles();
+                break;
+            case 'View all employees':
+                showEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployee();
+                break;
+            case 'View employees by department':
+                employeeDepartments();
+                break;
+            case 'Delete a department':
+                deleteDepartment();
+                break;
+            case 'Delete a role':
+                deleteRole();
+                break;
+            case 'Delete an employee':
+                deleteEmployee();
+                break;
+            case 'Exit':
+                connection.end();
+                break;
+            default:
+                break;
+        };
+    });
+};
