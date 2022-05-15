80df3c69e21f3c22787c9326aae29514a525998d
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-05-15 12:01:50 -0600

    update employee role function

--- server.js
+++ server.js
@@ -315,7 +315,63 @@ addEmployee=()=>{
     });
 };
 
+updateRole=()=>{
+    const employeeSql= `SELECT * FROM employee`;
 
+    connection.promise().query(employeeSql, (err, data)=>{
+        if(err) throw err;
+
+        const employees = data.map(({ id, first_name, last_name})=>({name: first_name + " " + last_name, value: id}));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: "Which employee would you like to update their role?",
+                choices: employees
+            }
+        ])
+        .then(empChoice=>{
+            const employee = empChoice.name;
+            const params = [];
+            params.push(employee);
+
+            const roleSql = `SELECT * FROM role`;
+
+            connection.promise().query(roleSql, (err, data)=>{
+                if (err) throw err;
+
+                const roles = data.map(({id, title})=>({name: title, value: id}));
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: "What is the employee's new role?",
+                        choices: roles
+                    }
+                ])
+                .then(roleChoice =>{
+                    const role = roleChoice.role;
+                    params.push(role);
+
+                    let employee = params[0];
+                    params[0] = role;
+                    params[1] = employee;
+
+                    const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+
+                    connection.query(sql, params, (err, result)=>{
+                        if(err) throw err;
+                        console.log('Employee role has been updated!');
+
+                        viewEmployee();
+                    });
+                });
+            });
+        });
+    });
+};
 
 
 
