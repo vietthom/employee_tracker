22518546442999b65074895145b0c28b68c24ad6
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-05-15 11:09:49 -0600

    updated viewDepartments, viewRoles, viewEmployees, addDept functions

---
+++ server.js
@@ -0,0 +1,174 @@
+const connection = require('./connection');
+const inquirer= require('inquirer');
+const cTable = require('console.table');
+
+const welcomeSign = () =>{
+    console.log("***********************************")
+    console.log("*                                 *")
+    console.log("*        EMPLOYEE MANAGER         *")
+    console.log("*                                 *")
+    console.log("***********************************")
+    menu();
+};
+
+welcomeSign();
+
+const menu = () =>{
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do?',
+            options: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department', 
+                'Add a role', 
+                'Add an employee', 
+                'Update an employee role', 
+                'Update an employee Manager', 
+                'View employees by department', 
+                'Delete a department', 
+                'Delete a role', 
+                'Delete an employee', 
+                'View department budgets',
+                'Exit'
+            ]
+        }
+    ])
+    .then((answer)=>{
+        const {options} = answer;
+
+        if (options === 'View all departments'){
+            viewDepartments();
+        }
+
+        if (options === 'View all roles'){
+            viewRoles();
+        }
+
+        if (options === 'View all employees'){
+            viewEmployees();
+        }
+
+        if(options === 'Add a department'){
+            addDepartment();
+        }
+
+        if(options === 'Add a role'){
+            addRole();
+        }
+
+        if(options === 'Add an employee'){
+            addEmployee();
+        }
+
+        if(options === 'Update an employee role'){
+            updateRole();
+        }
+
+        if(options === 'Update an employee manager'){
+            updateManager();
+        }
+
+        if(options === 'View employees by department'){
+            employeeDepartment();
+        }
+
+        if(options === 'Delete a department'){
+            deleteDepartment();
+        }
+
+        if(options === 'Delete a role'){
+            deleteRole();
+        }
+
+        if(options === 'Delete an employee'){
+            deleteEmployee();
+        }
+
+        if(option === 'View department budgets'){
+            viewBudget();
+        }
+
+        if(options === 'Exit'){
+            connection.end();
+        }
+    });
+};
+
+viewDepartments = () =>{
+    console.log('Gathering all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+    connection.promise().query(sql, (err,rows)=>{
+        if (err) throw err;
+        console.table(rows);
+        menu();
+    });
+};
+
+viewRoles = () =>{
+    console.log('Gathering all roles...\n');
+    const sql = `SELECT role.id, role.title, deparment.name AS department FROM role INNER JOIN department ON role.department_id = department.id`;
+    connection.promise().query(sql, (err,rows)=>{
+        if(err) throw err;
+        console.table(rows);
+        menu();
+    })
+};
+
+viewEmployees=()=>{
+    console.log('Gathering all employees...\n');
+    const sql = `SELECT employee.id,
+                        employee.first_name,
+                        employee.last_name,
+                        role.title, 
+                        department.name AS department, 
+                        role.salary, 
+                        CONCAT (manager.first_name, " ", manager.last_name) AS manager
+                FROM employee
+                        LEFT JOIN role ON employee.role_id=role.id
+                        LEFT JOIN department ON role.department_id = department.id
+                        LEFT JOIN employee manager ON employee.manager_id=manager.id`;
+    connection.promise().query(sql, (err,rows)=>{
+        if(err) throw err;
+        console.table(rows);
+        menu();
+    })
+}
+
+addDepartment=()=>{
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'addDept',
+            message: 'What deparment do you want to add?',
+            validate: addDept =>{
+                if(addDept){
+                    return true;
+                }else{
+                    console.log('Please enter a department name');
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(answer=>{
+        const sql = `INSERT INTO department (name)
+                    VALUES (?)`;
+        connection.query(sql, answer.addDept, (err, result)=>{
+            if (err) throw err;
+            console.log('Added ' + answer.addDept + ' to departments!');
+
+            viewDepartments();
+        });
+    });
+};
+
+
+
+
+
+
+
