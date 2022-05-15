0447471cc96aef312e2f7492049fe03751ad5cef
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-05-15 12:15:20 -0600

    update employee manager function

--- server.js
+++ server.js
@@ -373,6 +373,65 @@ updateRole=()=>{
     });
 };
 
+updateManger = ()=>{
+    const employeeSql = `SELECT * FROM employee`;
+
+    connection.promise().query(employeeSql, (err, data)=>{
+        if(err) throw err;
+
+        const employees = data.map(({id, first_name, last_name})=>({name: first_name + " " + last_name, value: id}));
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'name',
+                message: "Which employee would you like to update?",
+                choices: employees
+            }
+        ])
+        .then(empChoice =>{
+            const employee = empChoice.name;
+            const params =[];
+            params.push(employee);
+
+            const managerSql = `SELECT * FROM employee`;
+
+            connection.promise().query(managerSql, (err, data)=>{
+                if (err) throw err;
+
+                const manager = data.map(({id, first_name, last_name})=>({name: first_name + " " + last_name, value: id}));
+
+                inquirer.prompt([
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: "Who is the employee's manager?",
+                        choices: manager
+                    }
+                ])
+                .then(managerChoice =>{
+                    const manager = managerChoice.manager;
+                    params.push(manager);
+
+                    let employee = params[0];
+                    params[0] = manager;
+                    params[1]= employee;
+
+                    const sql = `UPDATE employee SET manager_id= ? WHERE id =?`;
+
+                    connection.query(sql, params, (err, result)=>{
+                        if (err) throw err;
+                        console.log("Employee's manager has been update!");
+
+                        viewEmployee();
+                    });
+                });
+            });
+        });
+    });
+};
+
+
 
 
 
